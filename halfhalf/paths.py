8465d1962700db63782225d1be56ba7e83d9d341
Katrina Owen <katrina.owen@gmail.com>
2026-03-26 06:25:27 +0100

    Add logging on translation failure

--- halfhalf/paths.py
+++ halfhalf/paths.py
@@ -69,3 +69,7 @@ def segment_breakpoints(episode_id):
 
 def translation_cache(episode_id, source_lang, target_lang):
     return os.path.join("output", episode_id, "data", f"translation.{source_lang}-{target_lang}.cache.json")
+
+
+def logs_dir(episode_id):
+    return os.path.join("output", episode_id, "logs")
